a35ba1f674c298ba889d788db4692ca8ff1afa8b
ckanth655 <31406146+ckanth655@users.noreply.github.com>
2019-02-28 15:53:09 +0530

    fgt

---
+++ springboot-example3/src/main/java/com/springboot/example/springbootexample3/SpringbootExample3Application.java
@@ -0,0 +1,12 @@
+package com.springboot.example.springbootexample3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootExample3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootExample3Application.class, args);
+	}
+}
